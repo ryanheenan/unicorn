d52535cb243cd30ff30f71196bdd769ffbadde0d
ryanheenan <101992238+ryanheenan@users.noreply.github.com>
2024-12-12 09:38:59 -0800

    Create input.tsx

---
+++ components/ui/input.tsx
@@ -0,0 +1,6 @@
+export const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => (
+  <input
+    {...props}
+    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+  />
+);
